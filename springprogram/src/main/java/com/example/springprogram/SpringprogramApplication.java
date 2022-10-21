b232f7a8fad6c1181dbe389cf203f2d514a08717
priyankasatpute24 <108567891+priyankasatpute24@users.noreply.github.com>
2022-10-21 09:54:55 +0530

    Add files via upload
    commit

---
+++ springprogram/src/main/java/com/example/springprogram/SpringprogramApplication.java
@@ -0,0 +1,13 @@
+package com.example.springprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprogramApplication.class, args);
+	}
+
+}
---
+++ springprogram/src/test/java/com/example/springprogram/SpringprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
